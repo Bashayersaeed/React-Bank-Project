4edb941d984849cb10483f7026abc4b2569c5726
Bashayersaeed <bsaeed83@ymail.com>
2024-11-27 18:43:04 +0300

    Doing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import Profile from './Components/Profile';
+import Transactions from './Components/Transactions';
 
 function App() {
 
+
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Transactions">Transactions</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
+  <div>
       <Routes>
         <Route path="/Register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
+    </div>
+  
   );
 }
 
